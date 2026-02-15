9827375ad22f4900029f077334e0d41ae830667e
Priyanshu Negi <113706789+Priyansh261@users.noreply.github.com>
2026-02-15 20:21:19 +0530

    Update main.tf

--- Terraformcode/modules/vpc/main.tf
+++ Terraformcode/modules/vpc/main.tf
@@ -71,13 +71,16 @@ resource "aws_route_table" "private" {
   count  = 3
   vpc_id = aws_vpc.main.id
 
-  route {
-    cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.nat[count.index].id
-  }
-
   tags = { Name = "private-rt-${count.index}" }
 }
+
+resource "aws_route" "private_nat" {
+  count                  = 3
+  route_table_id         = aws_route_table.private[count.index].id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.nat[count.index].id
+}
+
 
 # Private Associations
 resource "aws_route_table_association" "private" {
@@ -85,3 +88,4 @@ resource "aws_route_table_association" "private" {
   subnet_id      = aws_subnet.private[count.index].id
   route_table_id = aws_route_table.private[count.index].id
 }
+
